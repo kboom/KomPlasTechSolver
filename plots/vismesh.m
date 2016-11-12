9aa1a586b739984c1a01af798e6f41a43135e322
kboom <gurgul.grzegorz@gmail.com>
2016-11-12 11:35:00 +0100

    Added different results

--- plots/vismesh.m
+++ plots/vismesh.m
@@ -1,10 +1,9 @@
-function vistri(results_path)
+function vismesh(results_path)
     csv = csvread(results_path, 1, 0);
     x = csv(:,1);
     y = csv(:,2);
     z = csv(:,3);
-    tri = delaunay(x, y);
-    trimesh(tri, x, y, z);
+    mesh(x, y, z);
     title('Visualisation of problem solution')
     xlabel('x coordinate');
     ylabel('y coordinate');
