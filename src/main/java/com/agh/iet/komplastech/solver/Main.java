a807552e13e4a94f1c6ecf6e77c5d752a448eb30
kboom <gurgul.grzegorz@gmail.com>
2016-11-12 09:35:09 +0100

    Printing CSV to the standard output when main run.

--- src/main/java/com/agh/iet/komplastech/solver/Main.java
+++ src/main/java/com/agh/iet/komplastech/solver/Main.java
@@ -1,5 +1,7 @@
 package com.agh.iet.komplastech.solver;
 
+import com.agh.iet.komplastech.solver.results.CsvPrinter;
+
 import static com.agh.iet.komplastech.solver.support.Mesh.aMesh;
 
 public class Main {
@@ -12,7 +14,9 @@ public class Main {
                 .withResolutionY(12d)
                 .withOrder(2).build());
         try {
-            s.solveProblem();
+            Solution solution = s.solveProblem();
+            CsvPrinter csvPrinter = new CsvPrinter();
+            System.out.println(csvPrinter.convertToCsv(solution.getSolutionGrid()));
         } catch (Exception e) {
             e.printStackTrace();
         }
