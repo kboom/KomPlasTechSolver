ef757fab1d6e464c0ab3285ea34412e34dfa17c2
kboom <gurgul.grzegorz@gmail.com>
2017-02-25 20:08:18 +0100

    works only on vmware-based machine and network-mode = host

--- cluster/launch.sh
+++ cluster/launch.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-CFG_COMMAND="-e JAVA_OPTS="'-Dhazelcast.config=/config/hazelcast.xml'" -v /$(pwd)/config:/config"
+CFG_COMMAND="-e JAVA_OPTS="'-Dhazelcast.config=/config/hazelcast.xml'" -v /$(pwd)/config:/config --net=host -P"
 
-docker run -d -p 8080:8080 --name=hazelcast-management hazelcast/management-center
-# let management center start up on time
-sleep 10
+docker run -d --net=host -p 8080:8080 --name=hazelcast-management hazelcast/management-center
+#sleep 10
 
-# it is fine to expose only one member as they form the cluster and the client will know the others from the
-# single member of the cluster this member is in
-docker run -d $CFG_COMMAND -p 5701:5701 --name=hazelcast-member-1 kpts-hazelcast-member
+docker run -d $CFG_COMMAND --name=hazelcast-member-1 kpts-hazelcast-member
 docker run -d $CFG_COMMAND --name=hazelcast-member-2 kpts-hazelcast-member
