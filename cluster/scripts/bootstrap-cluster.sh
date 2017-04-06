8f9769d111e5571bfad39ffa806c7c9c27b32ab2
kboom <gurgul.grzegorz@gmail.com>
2017-04-06 20:16:23 +0200

    auto chmod scripts

--- cluster/scripts/bootstrap-cluster.sh
+++ cluster/scripts/bootstrap-cluster.sh
@@ -5,7 +5,7 @@ counter=0
 function restart_node {
     node_name="node$1"
     counter=$((counter+1))
-    ssh ${node_name} 'kill -9 $(/usr/sbin/lsof -t -i:5701) && ./solverCommands/restart-node.sh '${counter}'' &
+    ssh ${node_name} 'kill -9 $(/usr/sbin/lsof -t -i:5701) &> /dev/null && ./solverCommands/restart-node.sh '${counter}'' &
 }
 
 restart_node 2
