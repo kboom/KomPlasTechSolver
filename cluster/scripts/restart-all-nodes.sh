af7744bb69db4cdd5344a4c27a39d7f6d18309d6
kboom <gurgul.grzegorz@gmail.com>
2017-04-06 19:46:53 +0200

    Adding scripts for execution on cluster

--- cluster/scripts/restart-all-nodes.sh
+++ cluster/scripts/restart-all-nodes.sh
@@ -3,7 +3,7 @@ kinit
 
 counter=0
 function restart_node {
-    node_name=$(eval "node$1")
+    node_name="node$1"
     counter++
     bash ssh ${node_name} 'kill -9 $(/usr/sbin/lsof -t -i:5701) && ./restart-node.sh '${counter}'' &
 }
