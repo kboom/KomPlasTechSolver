fc9f98ba2645418881b6604ba87201edf7f2367c
kboom <gurgul.grzegorz@gmail.com>
2017-04-06 19:33:44 +0200

    Adding scripts for execution on cluster

--- cluster/scripts/update.sh
+++ cluster/scripts/update.sh
@@ -2,5 +2,5 @@
 rm -rf ./KptsSolverClient
 (cd $KPTS_SOLVER && git pull)
 mkdir -p .tmp
-tar -xvf $KPTS_SOLVER/dist/KomPlasTechSolver.tar -C .tmp/ && mv ./tmp/KomPlasTechSolver KptsSolverClient
+tar -xvf $KPTS_SOLVER/dist/KomPlasTechSolver.tar -C .tmp/ && mv .tmp/KomPlasTechSolver KptsSolverClient
 rm -rf .tmp
