058f00a001cdb8ae01c3448a8ef8eacf5b61f7c3
kboom <gurgul.grzegorz@gmail.com>
2017-04-06 19:48:56 +0200

    Adding scripts for execution on cluster

--- setup.sh
+++ setup.sh
@@ -15,7 +15,3 @@ ln -s ${SOLVER_ROOT}/cluster/config node-config
 export KPTS_SOLVER=${SOLVER_ROOT}
 export KPTS_SOLVER_DEPS=${SOLVER_ROOT}/dist
 export KPTS_SOLVER_CLIENT=$(pwd)/KptsSolverClient
-
-rm -f ~/setup-env.sh
-ln -s ${SOLVER_ROOT}/setup.sh ~/setup-env.sh
-chmod 775 ~/setup-env.sh
