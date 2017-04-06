dbb66d94304fdf278dda92f74932a5117c454712
kboom <gurgul.grzegorz@gmail.com>
2017-04-06 19:37:32 +0200

    Adding scripts for execution on cluster

--- cluster/scripts/run.sh
+++ cluster/scripts/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-solveCmd=$KPTS_SOLVER_CLIENT/KomPlasTechSolver.sh
+solveCmd=$KPTS_SOLVER_CLIENT/bin/KomPlasTechSolver
 
 env JAVA_OPTS="-Xms8G -Xmx12G" ${solveCmd} --problem-size $1 --steps 1 --log-process $2
