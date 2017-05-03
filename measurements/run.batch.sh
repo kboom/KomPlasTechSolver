939f6c88877cb6e528f58c54b3f8253f8c3a0137
kboom <gurgul.grzegorz@gmail.com>
2017-05-03 10:51:02 +0200

    added batch scripts

--- measurements/run.batch.sh
+++ measurements/run.batch.sh
@@ -5,7 +5,7 @@ PROBLEM_SIZES=(12 24 48 96 192 384 768 1536 3072 6144)
 function runBatch {
     PROBLEM_SIZE=$1
 
-	printf "\nRunning program with arguments problem size ($PROBLEM_SIZE)"
+	printf "\nRunning program with arguments problem size ($PROBLEM_SIZE)\n"
 
     ./run.sh ${PROBLEM_SIZE}
 }
