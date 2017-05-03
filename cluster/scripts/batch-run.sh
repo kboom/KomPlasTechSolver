ffc47f517fd2e9a6aacefe64d5689d8a529142f8
kboom <gurgul.grzegorz@gmail.com>
2017-05-03 18:42:14 +0200

    Added run results
    (cherry picked from commit d3129f7)

--- cluster/scripts/batch-run.sh
+++ cluster/scripts/batch-run.sh
@@ -11,14 +11,14 @@ BATCH_SIZE_STEP=${BATCH_SIZE_STEP-10}
 MAX_BATCH_SIZE=${MAX_BATCH_SIZE-100}
 
 function runBatch {
-    PROBLEM_SIZE=$1
-    REGION_HEIGHT=$2
-    BATCH_RATIO=$3
-    MAX_BATCH_SIZE=$4
+    T_PROBLEM_SIZE=$1
+    T_REGION_HEIGHT=$2
+    T_BATCH_RATIO=$3
+    T_MAX_BATCH_SIZE=$4
 
-	echo "Running program with arguments problem size ($PROBLEM_SIZE), region height ($REGION_HEIGHT), batch ratio ($BATCH_RATIO), max batch size ($MAX_BATCH_SIZE)"
+	echo "Running program with arguments problem size ($T_PROBLEM_SIZE), region height ($T_REGION_HEIGHT), batch ratio ($T_BATCH_RATIO), max batch size ($T_MAX_BATCH_SIZE)"
 
-    ./solverCommands/run.sh ${PROBLEM_SIZE} "--batch-ratio ${BATCH_RATIO} --max-batch-size ${MAX_BATCH_SIZE} --region-height ${REGION_HEIGHT}"
+    ./solverCommands/run.sh ${T_PROBLEM_SIZE} "--batch-ratio ${T_BATCH_RATIO} --max-batch-size ${T_MAX_BATCH_SIZE} --region-height ${T_REGION_HEIGHT}"
 }
 
 for ps in "${PROBLEM_SIZES[@]}"
