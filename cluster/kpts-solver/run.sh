ae84a966f072f8899a55f442701d2fe9e1c4b407
kboom <gurgul.grzegorz@gmail.com>
2017-05-03 10:08:59 +0200

    Preparing to run on plgrid

--- cluster/kpts-solver/run.sh
+++ cluster/kpts-solver/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -it -e JAVA_OPTS="-Xms6G -Xmx6G -Dcluster.address=10.0.0.3" --net=hazelcast-net kbhit/kpts-solver --problem-size $1 --steps 1 $2
+docker run -it -e JAVA_OPTS="-Xms6G -Xmx6G -Dcluster.address=10.0.0.3" --net=hazelcast-net -v /home/ubuntu/workdir:/workdir kbhit/kpts-solver --problem-size $1 --steps 1 $2
