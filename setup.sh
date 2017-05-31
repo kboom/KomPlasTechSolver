818eaf5fb0167f91642c51cde01321749c9fe550
kboom <gurgul.grzegorz@gmail.com>
2017-05-31 20:54:39 +0200

    Reordering exports

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get -y install openjdk-8-jre
+apt-get -y install \
+    openjdk-8-jre \
+    git-all
 
 SOLVER_ROOT=$(pwd)
 SCRIPTS_DIR=${SOLVER_ROOT}/cluster/scripts
+
+export KPTS_SOLVER=${SOLVER_ROOT}
+export KPTS_SOLVER_DEPS=${SOLVER_ROOT}/dist
+export KPTS_SOLVER_CLIENT=$(pwd)/KptsSolverClient
 
 cd ~
 bash ${SCRIPTS_DIR}/update.sh
@@ -13,7 +19,3 @@ rm -f solverCommands node-config dependencies
 ln -s ${SCRIPTS_DIR} solverCommands
 ln -s ${SOLVER_ROOT}/dist dependencies
 ln -s ${SOLVER_ROOT}/cluster/config node-config
-
-export KPTS_SOLVER=${SOLVER_ROOT}
-export KPTS_SOLVER_DEPS=${SOLVER_ROOT}/dist
-export KPTS_SOLVER_CLIENT=$(pwd)/KptsSolverClient
