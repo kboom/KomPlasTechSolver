ae8eb4eab90339d5a14c7c18016a800bad4646f5
kboom <gurgul.grzegorz@gmail.com>
2017-06-08 22:11:44 +0200

    Updating run command

--- cluster/scripts/run.sh
+++ cluster/scripts/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+CLUSTER_ADDRESS=$1
 solveCmd=$KPTS_SOLVER_CLIENT/bin/KomPlasTechSolver
 
-env JAVA_OPTS="-Xms6G -Xmx6G" ${solveCmd} --problem-size $1 --steps 1 $2
+env JAVA_OPTS="-Xms6G -Xmx6G -Dcluster.address=${CLUSTER_ADDRESS}" ${solveCmd} --problem-size $2 --steps 1 $3
