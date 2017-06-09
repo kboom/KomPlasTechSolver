7e443966116d9d254ae076532186d47d8349be9c
kboom <gurgul.grzegorz@gmail.com>
2017-06-09 21:50:31 +0200

    Enabling jmx monitoring

--- cluster/scripts/run.sh
+++ cluster/scripts/run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 CLUSTER_ADDRESS=$1
+STEPS=${4:-1}
+MIN_MEM=${5:-6G}
+MAX_MEM=${6:-6G}
 solveCmd=$KPTS_SOLVER_CLIENT/bin/KomPlasTechSolver
 
-env JAVA_OPTS="-Xms6G -Xmx6G -Dcluster.address=${CLUSTER_ADDRESS}" ${solveCmd} --problem-size $2 --steps 1 $3
+env JAVA_OPTS="-Xms${MIN_MEM} -Xmx${MAX_MEM} -Dcom.sun.management.jmxremote -Dhazelcast.jmx=true -Dcluster.address=${CLUSTER_ADDRESS}" ${solveCmd} --problem-size $2 --steps ${STEPS} $3
