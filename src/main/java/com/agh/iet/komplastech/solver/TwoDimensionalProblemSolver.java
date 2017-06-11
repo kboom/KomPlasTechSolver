19f05c5659c2413a8bfd1b64a2a315eac1b75cbd
kboom <gurgul.grzegorz@gmail.com>
2017-06-11 09:39:22 +0200

    Checking what happens if no solution is being read in Ay but partition martrices are still in place.

--- src/main/java/com/agh/iet/komplastech/solver/TwoDimensionalProblemSolver.java
+++ src/main/java/com/agh/iet/komplastech/solver/TwoDimensionalProblemSolver.java
@@ -100,18 +100,16 @@ class TwoDimensionalProblemSolver implements Solver {
     }
 
     private Solution solveProblemVertically(Solution horizontalSolution) {
-//        LeafInitializer verticalLeafInitializer = new VerticalLeafInitializer(launcherFactory, solutionLogger);
+        LeafInitializer verticalLeafInitializer = new VerticalLeafInitializer(launcherFactory, solutionLogger);
         ProductionFactory verticalProductionFactory = new VerticalProductionFactory(vertexRegionMapper);
         TreeIteratorFactory treeIteratorFactory = new TreeIteratorFactory();
-
-        HorizontalLeafInitializer horizontalLeafInitializer = new HorizontalLeafInitializer(launcherFactory, solutionLogger);
 
         DirectionSolver verticalProblemSolver = new DirectionSolver(
                 objectStore,
                 verticalProductionFactory,
                 launcherFactory,
                 treeIteratorFactory,
-                horizontalLeafInitializer,
+                verticalLeafInitializer,
                 mesh,
                 solutionLogger,
                 processLogger,
