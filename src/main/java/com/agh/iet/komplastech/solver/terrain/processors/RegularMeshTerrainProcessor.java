8c5f814255dd85310e7ba54c1d556b7487ceddcb
kbhit <gurgul.grzegorz@gmail.com>
2017-10-04 22:01:14 +0200

    Fixed KdTree implementation (entire double range rather than 0 to 1)

--- src/main/java/com/agh/iet/komplastech/solver/terrain/processors/RegularMeshTerrainProcessor.java
+++ src/main/java/com/agh/iet/komplastech/solver/terrain/processors/RegularMeshTerrainProcessor.java
@@ -2,37 +2,38 @@ package com.agh.iet.komplastech.solver.terrain.processors;
 
 import com.agh.iet.komplastech.solver.terrain.TerrainPoint;
 import com.agh.iet.komplastech.solver.terrain.TerrainProcessor;
-import com.agh.iet.komplastech.solver.terrain.support.KDTree;
+import com.agh.iet.komplastech.solver.terrain.support.KdTree2D;
+import com.agh.iet.komplastech.solver.terrain.support.Point2D;
 
 import java.util.stream.Stream;
 
 public class RegularMeshTerrainProcessor implements TerrainProcessor {
 
-    private KDTree kdTree;
+    private KdTree2D<Double> kdTree2D;
 
     @Override
     public void analyze(Stream<TerrainPoint> terrainPointStream) {
-        kdTree = new KDTree((int) terrainPointStream.count());
+        kdTree2D = new KdTree2D<>();
         terrainPointStream.forEach(terrainPoint -> {
-            kdTree.add(toListOfCoordinates(terrainPoint));
+            kdTree2D.insert(toPoint(terrainPoint));
         });
     }
 
     @Override
     public Stream<TerrainPoint> apply(Stream<TerrainPoint> terrainPointStream) {
         return terrainPointStream.map(terrainPoint -> {
-            double [] nearestCoordinates = kdTree.findNearest(toListOfCoordinates(terrainPoint));
-            return TerrainPoint.builder().x(nearestCoordinates[0]).y(nearestCoordinates[1]).z(terrainPoint.y).build();
+            Point2D<Double> nearestPoint = kdTree2D.nearest(toPoint(terrainPoint));
+            return new TerrainPoint(nearestPoint.x(), nearestPoint.y(), nearestPoint.getData());
         });
     }
 
     @Override
     public void teardown() {
-
+        kdTree2D = null;
     }
 
-    private double[] toListOfCoordinates(TerrainPoint terrainPoint) {
-        return new double[] { terrainPoint.x, terrainPoint.y };
+    private Point2D<Double> toPoint(TerrainPoint terrainPoint) {
+        return new Point2D<>(terrainPoint.x, terrainPoint.y, terrainPoint.z);
     }
 
 }
--- src/test/java/com/agh/iet/komplastech/solver/terrain/support/KDTreeTest.java
+++ src/test/java/com/agh/iet/komplastech/solver/terrain/support/KDTreeTest.java
@@ -8,12 +8,30 @@ public class KDTreeTest {
 
     @Test
     public void canFindNearest() {
-        KDTree kdTree = new KDTree(4);
-        kdTree.add(new double[] { 1, 2 });
-        kdTree.add(new double[] { 3, 4 });
-        kdTree.add(new double[] { 6, -6 });
-        kdTree.add(new double[] { 100, 4 });
-        assertThat(kdTree.findNearest(new double[] { 4, 5 })).isEqualTo(new double[] { 3, 4 });
+        KdTree2D kdTree2D = new KdTree2D();
+        kdTree2D.insert(new Point2D(1, 2));
+        kdTree2D.insert(new Point2D(3, 4));
+        kdTree2D.insert(new Point2D(6, -6));
+        kdTree2D.insert(new Point2D(-100, 4));
+        assertThat(kdTree2D.nearest(new Point2D(4,5))).isEqualTo(new Point2D(3, 4));
+    }
+
+    @Test
+    public void canFindNearest2() {
+        KdTree2D kdTree2D = new KdTree2D();
+        kdTree2D.insert(new Point2D(1, 2));
+        kdTree2D.insert(new Point2D(4, 4));
+        kdTree2D.insert(new Point2D(6, -6));
+        assertThat(kdTree2D.nearest(new Point2D(4,5))).isEqualTo(new Point2D(4, 4));
+    }
+
+    @Test
+    public void canFindNearest3() {
+        KdTree2D kdTree2D = new KdTree2D();
+        kdTree2D.insert(new Point2D(0, 0));
+        kdTree2D.insert(new Point2D(50, 50));
+        kdTree2D.insert(new Point2D(100, 100));
+        assertThat(kdTree2D.nearest(new Point2D(65,30))).isEqualTo(new Point2D(50, 50));
     }
 
 }
